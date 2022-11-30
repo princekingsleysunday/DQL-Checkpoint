539f6439762c7d2fbb7a281872e1318530e6558c
princekingsleysunday <114410713+princekingsleysunday@users.noreply.github.com>
2022-11-30 17:05:16 +0100

    Update DQL checkpoint.sql

--- DQL checkpoint.sql
+++ DQL checkpoint.sql
@@ -1,38 +1,38 @@
--- SELECT * FROM CUSTOMER;
+SELECT * FROM CUSTOMER;
 
--- SELECT PRODUCT_NAME, CATEGORY FROM PRODUCT WHERE PRICE BETWEEN 5000 AND 10000;
-
-
--- SELECT * FROM PRODUCT ORDER BY PRICE DESC;
+SELECT PRODUCT_NAME, CATEGORY FROM PRODUCT WHERE PRICE BETWEEN 5000 AND 10000;
 
 
--- SELECT SUM(QUANTITY),
--- AVG(TOTAL_AMOUNT),
--- MAX(TOTAL_AMOUNT),
--- MIN(TOTAL_AMOUNT);
+SELECT * FROM PRODUCT ORDER BY PRICE DESC;
 
 
--- SELECT DISTINCT PRODUCT_ID, QUANTITY FROM ORDERS;
+SELECT SUM(QUANTITY),
+AVG(TOTAL_AMOUNT),
+MAX(TOTAL_AMOUNT),
+MIN(TOTAL_AMOUNT);
 
 
--- SELECT CUSTOMER_ID FROM ORDERS WHERE QUANTITY > 2;
+SELECT DISTINCT PRODUCT_ID, QUANTITY FROM ORDERS;
 
 
-
-        For each month of 2020 year, display the number of orders
--- SELECT orderdate from orders group by orderdate having group by orderdate extract('month' from orderdate) where year = '2020';
+SELECT CUSTOMER_ID FROM ORDERS WHERE QUANTITY > 2;
 
 
-
--- select product.product_name, customer.customer_name, orderdate from orders 
--- inner join product on orders.product_id =product.product_id
--- inner join customer on orders.customer_id = customer.customer_id
-
-
--- select * from orders where orderdate > current_date - interval '3 months'
+select extract(month from order_date) as month, 
+extract(year from order_date) as year, count(order_id) 
+from orders order by month where year= '2020';
 
 
 
--- SELECT customer_name FROM Customer LEFT JOIN Orders ON customer.customer_id = orders.Customer_id WHERE orders.Customer_id IS NULL
+select product.product_name, customer.customer_name, orderdate from orders 
+inner join product on orders.product_id =product.product_id
+inner join customer on orders.customer_id = customer.customer_id
+
+
+select * from orders where orderdate > current_date - interval '3 months'
+
+
+
+select cust_id from orders not in (select cust_id from customer);
 
 
